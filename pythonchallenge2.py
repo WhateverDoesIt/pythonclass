9a909670b8342ec7797f9bac47aa111000e4fca7
WhateverDoesIt <36092226+WhateverDoesIt@users.noreply.github.com>
2018-02-02 21:16:05 -0500

    Update pythonchallenge2.py

--- pythonchallenge2.py
+++ pythonchallenge2.py
@@ -1,4 +1,3 @@
-#thestring = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 thestring = "test string"
 
 letter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
